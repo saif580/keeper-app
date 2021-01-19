cf855d67d5dc337b4a6b0af34e7a7b5b6eb281fa
iamsaif <saifulhasan580@gmail.com>
2021-01-19 15:29:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer(){
+    return <footer><p>Keeper App 2021 &#169; saif</p></footer>
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note(){
+    return (
+    <div className="note">
+        <h1>Note Title</h1>
+        <p>Hi there! My name is saif</p>
+    </div>
+    );
+}
+
+export default Note;
